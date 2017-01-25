a9329061e606048dd91b270ff61a2dff5086c05e
traverseda <traverse.da@gmail.com>
2017-01-25 06:37:19 -0400

    Lots of utility functions. Logging still needs major work.

--- akashic/db.py
+++ akashic/db.py
@@ -75,3 +75,79 @@ def addConnToParser(parser):
 def connectFromArgs(args):
     connectionArgs = {k:v for k,v in vars(args).items() if k in ('db','user','port','password','host')}
     return r.connect(**connectionArgs)
+
+import logger
+
+def HyperLogger():
+    """
+    A logger that logs to rethinkdb, if initialized with a connection.
+    It passes through to standard python logging.
+    """
+    self.loglevels = ('critical','error','warning','info','debug')
+    def __init__(self, conn=None, passthrough=logger):
+        self.conn=conn
+        self.passthrough=passthrough
+    def log(self, msg, **data):
+        data['ctime']=arrow.utcnow()
+        if self.passthrough:
+            for level in self.loglevels:
+                call = getattr(foo, 'bar')
+    def CRITICAL(self, msg, **data):
+        data['tags'].append("critical")
+        self.log(msg, **data)
+    def ERROR(self, msg, **data):
+        data['tags'].append("error")
+        self.log(msg, **data)
+    def WARNING(self, msg, **data):
+        data['tags'].append("warning")
+        self.log(msg, **data)
+    def INFO(self, msg, **data):
+        data['tags'].append("info")
+        self.log(msg, **data)
+    def DEBUG(self, msg, **data):
+        data['tags'].append("debug")
+        self.log(msg, **data)
+
+from collection import set
+import atexit, logging, arrow, asyncio
+class ExitHandler(set):
+    """
+    An easy way to clean up temporary data,
+    which I expect to use a lot.
+    Also adds an "heartbeat" field to all objects contained within it, in case we don't shut
+    down cleanly. This would let a daemon handle all cleanup.
+
+    You must either use as asyncio.run_forever() or add ExitHandler().heartBeat() to your async framework
+    to use heartbeats. heartBeat() should be run approxametly every 30 seconds. The default cleanup daemon
+    should be set to clean up objects older then 60 seconds.
+
+    If you can't use heartbeats you really shouldn't be making use of temporary objects.
+    """
+    def __init__(self, connection, *args, **kwargs):
+        self.heartbeat=None
+        self.conn=connection
+        atexit.register(self.atExit)
+        asyncio.async(self.asyncHeartBeat())
+        super().__init__(self, *args, **kwargs)
+    def atExit(self):
+        for item in self:
+            item.delete()
+    def async asyncHeartBeat(self):
+        while True:
+            self.heartBeat()
+            await asyncio.sleep(30)
+
+    def heartBeat(self):
+        now = arrow.utcnow()
+        errors = []
+        for item in self:
+            item.update({'heatbeat':now}).run(self.conn)
+        interval = int(now)-int(self.heartbeat)
+        if errors:
+            logging.ERROR("{objectCount} objects were cleaned up, but {program} still needs them. This is probably due to a bad heatbeat in {program}.".format(
+                objectCount = len(errors),
+                program="PLACEHOLDER"
+            )
+        if self.heatBeat and interval > 60:
+            logging.WARNING("Took {} seconds refresh heatbeat. Max recomended in 60.".format(interval))
+        self.heartbeat=now
