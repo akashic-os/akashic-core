05da432351dd31af68c119a3ad44df23ce7a8f4c
traverseda <traverse.da@gmail.com>
2017-01-25 03:25:44 -0400

    Added generic "connectionParser" for argparse. Added --list-table and --list-dbs arguments for rq.

--- akashic/db.py
+++ akashic/db.py
@@ -1,5 +1,6 @@
 import rethinkdb as r
 
+
 mainDB = 'akashic'
 
 tables = {
@@ -39,5 +40,38 @@ def ensureDB():
             newindexes =  [index for index in indexes[key] if index not in  r.table(key).index_list().run()]
             for index in newindexes:
                 r.table(key).index_create(index).run()
+#ensureDB()
+import os
+from collections import ChainMap
 
-ensureDB()
+localDefaults={
+'akashic_db':'akashic',
+'akashic_user':'admin',
+'akashic_port':'28015',
+'akashic_password':'',
+'akashic_host':'localhost',
+}
+
+defaultSettings=ChainMap(os.environ, localDefaults)
+
+def addConnToParser(parser):
+    """
+    Takes a parser objects, adds options for connectiong,
+    Connects.
+    Returns a query object. In the future that may change.
+    """
+    parser.add_argument('--db', type=str,
+                    help='Which db to edit', default=defaultSettings['akashic_db'])
+    parser.add_argument('--user', type=str,
+                    help='Which user to log in as', default=defaultSettings['akashic_user'])
+    parser.add_argument('--port', type=str,
+                    help="The port of your rethinkDB server",
+                    default=defaultSettings['akashic_port'])
+    parser.add_argument('--password', type=str,
+                    default=defaultSettings['akashic_password'])
+    parser.add_argument('--host', type=str,
+                    help='The address of you rethinkDB server', default=defaultSettings['akashic_host'])
+
+def connectFromArgs(args):
+    connectionArgs = {k:v for k,v in vars(args).items() if k in ('db','user','port','password','host')}
+    return r.connect(**connectionArgs)
